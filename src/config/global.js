26136cda478740a3b9718873f92cb900761c72dd
Andrea Botello <abotellod@gmail.com>
2026-04-13 12:30:11 -0500

    Video y Podcast

--- src/config/global.js
+++ src/config/global.js
@@ -150,7 +150,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF1_72310003_DU.pdf',
       },
       {
         icono: 'fas fa-download',
@@ -326,8 +326,8 @@ export default {
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
-          nombre: ' ',
-          cargo: 'Animador y productor audiovisual',
+          nombre: 'Maria Alejandra Vera Briceño',
+          cargo: 'Animadora y productora audiovisual',
           centro: 'Centro Agroturístico - Regional Santander',
         },
       ],
@@ -341,7 +341,7 @@ export default {
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
-          nombre: ' ',
+          nombre: 'Sandra Liliana Cristancho Cruz',
           cargo: 'Evaluadora de contenidos inclusivos y accesibles',
           centro: 'Centro Agroturístico - Regional Santander',
         },
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -242,8 +242,8 @@
               p.mb-4 Para ampliar esta información se invita a explorar el siguiente video:
               figure
                 .video
-                  iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-                figcaption Video. 
+                  iframe(width="560" height="315" src="https://www.youtube.com/embed/m2VjhfjLKKw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+                figcaption Video. Producción limpia y gestión ambiental en sistemas agropecuarios 1 
 
 
 
